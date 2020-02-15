444bf4b5c9296efdcb93f8ea4bf9a7adae6b4b09
Andrey Li <lee1975uz@gmail.com>
2020-02-14 22:45:16 -0700

    add fonts, logo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-3 p-5">
+      <img
+        src="https://instagram.fyyc4-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fyyc4-1.fna.fbcdn.net&_nc_ohc=aQEJO6e_QoIAX_Ji7iY&oh=67a1952ccfa409265f6de6568830da2e&oe=5ECEBCBA"
+        class="rounded-circle" alt="logo">
+    </div>
+    <div class="col-9 pt-5">
+      <div class="d-flex">
+        <h1>Line Laravel</h1>
+      </div>
+      <div class="d-flex">
+        <div class="pr-5"><strong>23k</strong> followers</div>
+        <div class="pr-5"><strong>153</strong> posts</div>
+        <div class="pr-5"><strong>212</strong> following</div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
