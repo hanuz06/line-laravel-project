b46578e524c68c6fcd83d3fc3d92d118528e3b98
Andrey Li <lee1975uz@gmail.com>
2020-02-25 10:59:19 -0700

    add caching to profiles controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -5,12 +5,34 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Cache;
 
 class ProfilesController extends Controller
 {
     public function index(User $user)
     {
         $follows = (auth()->user())? auth()->user()->following->contains($user->id) : false;
+
+        $postCount = Cache::remember(
+            'count.posts.'.$user->id,
+            now()->addSeconds(30),
+            function() use ($user) {
+                return $user->posts->count();
+            });
+
+        $followersCount = Cache::remember(
+            'count.followersCount.'.$user->id,
+            now()->addSeconds(30),
+            function() use ($user) {
+                return $user->profile->followers->count();
+            });
+
+        $followingCount = Cache::remember(
+            'count.followingCount.'.$user->id,
+            now()->addSeconds(30),
+            function() use ($user) {
+                return $user->following->count();
+            });
 
         // dd($follows);
 
@@ -20,7 +42,7 @@ class ProfilesController extends Controller
         // return view('profiles.index', [
         //     'user' => $user,
         // ]);
-        return view('profiles.index', compact('user', 'follows'));
+        return view('profiles.index', compact('user', 'follows','postCount','followersCount', 'followingCount'));
     }
 
     public function edit(User $user)
