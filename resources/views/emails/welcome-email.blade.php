26cb1fae22d76eef16d848fcb862963515e818cd
Andrey Li <lee1975uz@gmail.com>
2020-02-25 14:45:38 -0700

    upload pics for readme

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome to Line
+
+The Thanksgiving potluck will be held in the church gymnasium right after the 2nd sermon. Bring a dish to share and
+enjoy the fellowship!
+
+All the best,<br>
+Mark
+@endcomponent
